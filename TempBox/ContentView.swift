b16d67e8b27d825a55cbb168121d7dfda9729f79
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-23 12:30:47 +0530

    Initial Commit

---
+++ TempBox/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TempBox
+//
+//  Created by Rishi Singh on 23/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TempBox/TempBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempBoxApp.swift
+//  TempBox
+//
+//  Created by Rishi Singh on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TempBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
